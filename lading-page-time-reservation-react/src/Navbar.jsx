5aa7d0b534f3ff3f9553042cb93646dc9569d466
F4fu <bensamuel114@gmail.com>
2022-11-17 21:39:31 -0500

    end-nav-var

--- lading-page-time-reservation-react/src/Navbar.jsx
+++ lading-page-time-reservation-react/src/Navbar.jsx
@@ -1,3 +1,19 @@
+import "./styles.css";
+import imglogo from './img/logo/logo.png'
+
+
+function Logo() {
+  return (
+    <div className="logo">
+      <img
+        src={imglogo}
+        alt="Logo Full Time Reservation"
+      ></img>
+    </div>
+  );
+}
+
+
 function TokenList(props) {
   return (
     <li>
@@ -8,34 +24,42 @@ function TokenList(props) {
 
 function List() {
   return (
-    <>
+    <nav>
       <ul className="nav-list">
         <TokenList name="Inicio" />
         <TokenList name="Restaurantes" />
         <TokenList name="Promociones" />
       </ul>
-    </>
+    </nav>
   );
 }
 
-function ButtonNavbar(props){
-    return <>
-        <a href="#" class="btn"><button>{props.name}</button></a>
-    </>
+function ButtonNavbar(props) {
+  return (
+    <a href="#" className="btn">
+      <button>{props.name}</button>
+    </a>
+  );
 }
-function ListButtonNavbar(){
-    return (
+function ListButtonNavbar() {
+  return (
     <div className="list-btn">
-        <ButtonNavbar name="Iniciar Sesion"/>
-        <ButtonNavbar name="Registro"/>
-    </div>)
+      <ButtonNavbar name="Iniciar Sesion" />
+      <ButtonNavbar name="Registrarse" />
+    </div>
+  );
 }
 
 function Navbar() {
-  return <>
-  <List/>
-  <ListButtonNavbar/>
-  </>;
+  return (
+    <body>
+      <header className="header">
+        <Logo />
+        <List />
+        <ListButtonNavbar />
+      </header>
+    </body>
+  );
 }
 
 export default Navbar;
