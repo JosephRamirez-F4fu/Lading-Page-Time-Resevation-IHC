ec0a5d27b91528697ab404f2337d54ae160009c1
F4fu <bensamuel114@gmail.com>
2022-11-18 00:03:26 -0500

    doing-more-visited

---
+++ lading-page-time-reservation-react/src/Visited.jsx
@@ -0,0 +1,29 @@
+function CircleImage(props) {
+  return (
+    <div className="rest-img">
+      <img className="circle" alt="img" src={props.img}></img>
+      <h3>{props.text}</h3>
+    </div>
+  );
+}
+
+function CircleGallery() {
+  return <div className="circle-gallery">
+    <CircleImage img="" text="hola"/>
+    <CircleImage img="" text="gola"/>
+    <CircleImage img="" text="tea"/>
+  </div>;
+}
+
+function Visited() {
+  return (
+    <div className="Restaurantes-buscados">
+      <h2 className="contenedor">
+        Los Restaurantes más visitados en la semana
+      </h2>
+      <CircleGallery/>
+    </div>
+  );
+}
+
+export default Visited;
--- lading-page-time-reservation-react/src/index.js
+++ lading-page-time-reservation-react/src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from "react-dom/client";
 import Navbar from "./Navbar";
 import Collage from "./Collage";
 import Footer from "./Footer";
+import Visited from "./Visited";
 import "./styles.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
     <body>
       <Navbar />
+      <Visited/>
       <Collage />
     </body>
-    <Footer></Footer>
+    <Footer/>
   </>
 );
