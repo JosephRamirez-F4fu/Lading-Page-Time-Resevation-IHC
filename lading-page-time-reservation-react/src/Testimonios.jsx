2834ae5a95b4079569e36179b97f12b7cf45e92e
F4fu <bensamuel114@gmail.com>
2022-11-18 00:15:29 -0500

    add comentary

--- lading-page-time-reservation-react/src/Testimonios.jsx
+++ lading-page-time-reservation-react/src/Testimonios.jsx
@@ -25,3 +25,31 @@ function TokenList(props) {
   }
   
   export default Testimonios;
+
+  /*
+  <div class="Testimonios">
+        
+        
+        <div class="test-title">
+            <h3> Testimonios</h3>
+            <p>Conoce las opiniones de nuestros usuarios</p>
+        </div>  
+        
+        <div class="test-row">
+            <div class="Testimonio">
+                <h1 class="test-client">Cliente </h1>
+                <div class="cont-test-img-text">
+                    <img alt="img-test" src="/img/Clientes/1.jpg">
+                    <p>"Me encanta su página web, así puedo realizar mis reservas rápidamente y saber cual me convendría"</p>
+                </div>
+            </div>
+
+            <div class="Testimonio">
+                <h1 class="test-client" >Dueño </h1>
+                <div class="cont-test-img-text">
+                    <img alt="img-test" src="/img/Dueños/1.jpg">
+                    <p>"Gracias a su página web logré promocionar mi restaurante y conseguir mayor cantidad de clientes para mi local"</p>
+                </div>
+            </div>    
+        </div>
+  */
